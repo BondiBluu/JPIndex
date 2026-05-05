bf2c07a2a7f90a452721f4802f415732b13f709f
Bamidele Ogundeji <developers@previouslypluto.com>
2026-05-05 15:49:20 -0400

    Update UpdateScript to include connection string parameter and enhance record update functionality

--- jpIndexer/Scripts/UpdateScript.cs
+++ jpIndexer/Scripts/UpdateScript.cs
@@ -1,8 +1,21 @@
+using Microsoft.Data.Sqlite;
+
 public static class UpdateScript
 {
     public static void Update(string connectionString)
     {
         Console.Clear();
         ViewScript.GetRecords(connectionString);
+
+        string wordToSearch = InsertScript.GetStringInput("\n\nType the English word or phrase of the row you'd like to update. Type 0 to return to the Main Menu.\n\n");
+
+        using(SqliteConnection connection = new SqliteConnection(connectionString))
+        {
+            connection.Open();
+
+            SqliteCommand checkCmd = connection.CreateCommand();
+
+            checkCmd.CommandText = $"SELECT EXISTS(SELECT 1 FROM japanese_index WHERE English = {wordToSearch})";
+        }
     }
 }
--- jpIndexer/obj/Debug/net9.0/jpIndexer.AssemblyInfo.cs
+++ jpIndexer/obj/Debug/net9.0/jpIndexer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("jpIndexer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7bb2d4ae3c7c29136e06e4c0b0566045b9d82a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d7a87c963982ed66f954670ae1349bfb76bf4fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("jpIndexer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("jpIndexer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
